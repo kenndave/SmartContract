3bb679a70717f825370cb45ae0754432260e354d
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-12-23 19:36:29 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,53 @@
 // Smart Contract: Decentralized Locker System
 import {
-    query,
-    update,
-    text,
-    Record,
-    StableBTreeMap,
-    Principal,
-    nat64,
-    Canister,
-    Err,
-    Ok,
-    Result,
-    Vec,
-    bool,
-    ic,
-    Opt,
-    None,
-    Some
-  } from "azle";
+  query,
+  update,
+  text,
+  Record,
+  StableBTreeMap,
+  Principal,
+  nat64,
+  Canister,
+  Err,
+  Ok,
+  Result,
+  Vec,
+  bool,
+  ic,
+  Opt,
+  None,
+  Some,
+} from "azle";
 
 const Renter = Record({
   id: Principal,
   userRenterId: Principal,
   duration: nat64,
-  lockerId: Principal
-})
+  lockerId: Principal,
+});
 
 const RenterPayload = Record({
   duration: nat64,
-  lockerId: Principal
-})
+  lockerId: Principal,
+});
 const Locker = Record({
-  id: Principal,
+  lockerId: Principal, // Rename `id` to `lockerId` for clarity
   number: text,
   availability: bool,
-  renterId: Opt(Principal)
-})
+  renterId: Opt(Principal),
+});
 const LockerPayload = Record({
-  number: text
-})
+  number: text,
+});
 const User = Record({
   id: Principal,
   name: text,
-  createdAt: nat64
-})
+  createdAt: nat64,
+});
 
 const UserPayload = Record({
-  name: text
-})
+  name: text,
+});
 
 type Renter = typeof Renter.tsType;
 type RenterPayload = typeof RenterPayload.tsType;
@@ -60,188 +60,182 @@ let lockers = StableBTreeMap<Principal, Locker>(1);
 let rents = StableBTreeMap<Principal, Renter>(2);
 
 export default Canister({
-    createUser: update([UserPayload], Result(User, text), (payload) => {
-      // Input validation
-      if (!payload.name){
-        return Err("Input invalid");
-      }
-      const id = generateId();
-      const user: User = {
-        id,
-        name : payload.name,
-        createdAt : ic.time()
-      };
-      users.insert(user.id, user);
+  createUser: update([UserPayload], Result(User, text), (payload) => {
+    // Input validation
+    if (!payload.name) {
+      return Err("Input invalid");
+    }
+    const id = generateId();
+    const user: User = {
+      id,
+      name: payload.name,
+      createdAt: ic.time(),
+    };
+    users.insert(user.id, user);
 
-      return Ok(user);
-    }),
+    return Ok(user);
+  }),
 
-    getUsers: query([], Vec(User), () =>{
-      return users.values();
-    }),
+  getUsers: query([], Vec(User), () => {
+    return users.values();
+  }),
 
-    getUserById: query([Principal], Result(User, text), (userID) => {
-      const userOpt = users.get(userID); 
-      if ("None" in userOpt){
-        return Err("User doesnt exist")
-      }
+  getUserById: query([Principal], Result(User, text), (userID) => {
+    const userOpt = users.get(userID);
+    if ("None" in userOpt) {
+      return Err("User doesnt exist");
+    }
 
-      return Ok(userOpt.Some);
-    }),
+    return Ok(userOpt.Some);
+  }),
 
-    getLockers: query([], Vec(Locker), () => {
-      return lockers.values();
-    }),
-    getLockerById: query([Principal], Result(Locker, text), (lockerID) => {
-      const lockerOpt = lockers.get(lockerID); 
-      if ("None" in lockerOpt){
-        return Err("Locker doesnt exist");
-      }
+  getLockers: query([], Vec(Locker), () => {
+    return lockers.values();
+  }),
+  getLockerById: query([Principal], Result(Locker, text), (lockerID) => {
+    const lockerOpt = lockers.get(lockerID);
+    if ("None" in lockerOpt) {
+      return Err("Locker doesnt exist");
+    }
 
-      return Ok(lockerOpt.Some);
-    }),
+    return Ok(lockerOpt.Some);
+  }),
 
-    getRenters: query([], Vec(Renter), () =>{
-      return rents.values();
-    }),
-    getRenterById: query([Principal], Result(Renter, text), (renterID) => {
-      const renterOpt = rents.get(renterID); 
-      if ("None" in renterOpt){
-        return Err("Renter doesnt exist");
-      }
+  getRenters: query([], Vec(Renter), () => {
+    return rents.values();
+  }),
+  getRenterById: query([Principal], Result(Renter, text), (renterID) => {
+    const renterOpt = rents.get(renterID);
+    if ("None" in renterOpt) {
+      return Err("Renter doesnt exist");
+    }
 
-      return Ok(renterOpt.Some);
-    }),
-    addLocker: update([LockerPayload], Result(Locker, text), (payload) =>{
-      // Validate inputs
-      if (!payload.number){
-        return Err("Input invalid");
-      }
+    return Ok(renterOpt.Some);
+  }),
+  addLocker: update([LockerPayload], Result(Locker, text), (payload) => {
+    // Validate inputs
+    if (!payload.number) {
+      return Err("Input invalid");
+    }
 
+    let id = generateId();
+    let existLockers = lockers.get(id);
+    while (!("None" in existLockers)) {
+      id = generateId();
+      existLockers = lockers.get(id);
+    }
+    const locker: Locker = {
+      lockerId: id, // Change `id` to `lockerId` for consistency
+      number: payload.number,
+      availability: true,
+      renterId: None,
+    };
+    lockers.insert(locker.lockerId, locker);
+    return Ok(locker);
+  }),
 
+  rentLocker: update([Principal, RenterPayload], Result(Renter, text), (userId, payload) => {
+    // Input validation
+    if (!payload.duration || !payload.lockerId) {
+      return Err("Input invalid");
+    }
+    // Checking user and locker existence
 
-      let id = generateId();
-      let existLockers = lockers.get(id);
-      while(!("None" in existLockers)){
-        id = generateId();
-        existLockers = lockers.get(id);
-      }
-      const locker: Locker = {
-        id,
-        number: payload.number,
-        availability: true,
-        renterId: None
-      }
-      lockers.insert(locker.id, locker);
-      return Ok(locker);
-    }),
+    if (!userId) {
+      return Err("Invalid user");
+    }
 
-    rentLocker: update([Principal, RenterPayload], Result(Renter, text), (userId, payload) => {
-      // Input validation
-      if (!payload.duration || !payload.lockerId){
-        return Err("Input invalid");
-      }
-      // Checking user and locker existance
-      
-      if (!userId){
-        return Err("Invalid user");
-      }
+    if (!lockers.get(payload.lockerId)) {
+      return Err("Locker does not exist!");
+    }
 
+    // Checking if locker is valid and available
+    const lockerOpt = lockers.get(payload.lockerId);
+    // Validate locker existence
+    if ("None" in lockerOpt) {
+      return Err("Locker doesn't exist");
+    }
+    const locker = lockerOpt.Some;
+    // Validate locker availability
+    if (!locker.availability) {
+      return Err("Locker is not available");
+    }
 
-      if (!lockers.get(payload.lockerId)){
-        return Err("Locker does not exist!");
-      }
+    // Create Rent & since locker is available
+    let rentId = generateRentId(userId);
+    let existRents = rents.get(rentId);
+    while (!("None" in existRents)) {
+      rentId = generateId();
+      existRents = rents.get(rentId);
+    }
+    const renter: Renter = {
+      id: rentId,
+      userRenterId: userId,
+      duration: payload.duration,
+      lockerId: payload.lockerId,
+    };
 
-      // Checking if locker is valid and available
-      const lockerOpt = lockers.get(payload.lockerId)
-      // Validate locker existance
-      if ("None" in lockerOpt){
-        return Err("Locker doesn't exist");
-      }
-      const locker = lockerOpt.Some;
-      // Validate locker availability
-      if (!locker.availability){
-        return Err("Locker is not available");
-      }
+    // Update Locker
+    const usedLocker: Locker = {
+      ...locker,
+      availability: false,
+      renterId: Some(payload.lockerId),
+    };
 
-      // Create Rent & since locker is available
-      let rentId = generateRentId(userId);
-      let existRents = rents.get(rentId);
-      while(!("None" in existRents)){
-        rentId = generateId();
-        existRents = rents.get(rentId);
-      }
-      const renter: Renter ={
-        id: rentId,
-        userRenterId: userId,
-        duration: payload.duration,
-        lockerId: payload.lockerId
-      }
+    rents.insert(renter.id, renter);
+    lockers.insert(payload.lockerId, usedLocker);
+    return Ok(renter);
+  }),
 
-      // Update Locker
-      const usedLocker: Locker = {
-        ...locker,
-        availability: false,
-        renterId: Some(payload.lockerId),
-      }
+  evictLocker: update([Principal], Result(bool, text), (rentalID) => {
+    // Validation of rented locker
+    if (!rentalID) {
+      return Result.Err("Locker is still available");
+    }
 
-      rents.insert(renter.id, renter);
-      lockers.insert(payload.lockerId, usedLocker)
-      return Ok(renter);
+    // Checking if locker is unavailable and used by user
+    const rentalOpt = rents.get(rentalID);
+    if ("None" in rentalOpt) {
+      return Err("Locker is still available");
+    }
+    const rental = rentalOpt.Some;
+    if (!rental) {
+      return Err("Locker rental was not valid");
+    }
+    // Getting locker information
+    const lockerOpt = lockers.get(rental.lockerId);
+    // Validate locker existence
+    if ("None" in lockerOpt) {
+      return Err("Locker doesn't exist");
+    }
+    const locker = lockerOpt.Some;
 
-    }),
+    // Update locker
+    const usedLocker: Locker = {
+      ...locker,
+      availability: true,
+      renterId: None,
+    };
 
-    evictLocker: update([Principal], Result(bool, text), (rentalID) => {
-      // Validation of rented locker
-      if (!rentalID){
-        return Result.Err("Locker is still available");
-      }
-
-      // Checking if locker is unavailable and used by user
-      const rentalOpt = rents.get(rentalID)
-      if ("None" in rentalOpt){
-        return Err("Locker is still available");
-      }
-      const rental = rentalOpt.Some;
-      if (!rental){
-        return Err("Locker rental was not valid");
-      }
-      // Getting locker information
-      const lockerOpt = lockers.get(rental.lockerId);
-      // Validate locker existance
-      if ("None" in lockerOpt){
-        return Err("Locker doesn't exist")
-      }
-      const locker = lockerOpt.Some;
-    
-      // Update locker
-      const usedLocker: Locker = {
-        ...locker,
-        availability: true,
-        renterId: None,
-      }
-
-      lockers.insert(locker.id, usedLocker);
-      // Removing rental information, since locker has been released
-      rents.remove(rentalID);
-      return Result.Ok(true);
-    })
+    lockers.insert(locker.lockerId, usedLocker);
+    // Removing rental information, since locker has been released
+    rents.remove(rentalID);
+    return Result.Ok(true);
+  }),
 });
 
 function generateId(): Principal {
   const randomBytes = new Array(29)
-      .fill(0)
-      .map((_) => Math.floor(Math.random() * 256));
+    .fill(0)
+    .map((_) => Math.floor(Math.random() * 256));
 
   return Principal.fromUint8Array(Uint8Array.from(randomBytes));
 }
 function generateRentId(userId: Principal): Principal {
   const timestampBytes = new Array(13).fill(0).map((_) => Number(ic.time()) % 256);
   const randomBytes = new Array(16)
-      .fill(0)
-      .map((_) => Math.floor(Math.random() * 256));
+    .fill(0)
+    .map((_) => Math.floor(Math.random() * 256));
   const combinedBytes = [...timestampBytes, ...randomBytes];
   return Principal.fromUint8Array(Uint8Array.from(combinedBytes));
 }
-
-
